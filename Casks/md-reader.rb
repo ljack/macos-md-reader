6b91891a4bfaa1a04cb4b4380077b024aeab2a6f
Jarkko Lietolahti <Jarkko.lietolahti@gmail.com>
2026-09-13 17:36:39 +0300

    Release 0.1.0

--- Casks/md-reader.rb
+++ Casks/md-reader.rb
@@ -1,6 +1,6 @@
 cask "md-reader" do
   version "0.1.0"
-  sha256 "cd83dbeda07a5a82396711566cdfeb307cc0af446e0927e95f7faa09f7fb0b51"
+  sha256 "8f4db5c0aa314439b5db92d2dccd54aa98f293857a7ab7fc2a1a3f0543031124"
 
   url "https://github.com/ljack/macos-md-reader/releases/download/v#{version}/MD-Reader-#{version}.zip"
   name "MD Reader"
